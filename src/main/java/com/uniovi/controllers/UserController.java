7928aee1e1be667c316eec7bc83e7b06a72cb1ee
MiguelOrnia <mor-nia@hotmail.com>
2019-03-06 19:34:03 +0100

    4 - Añadiendo vistas, y caso de uso 1 (Registro de un usuario)
    Se ha añadido las vistas (Home, Signup e Index), cuenta con el caso 1 (registro de un usuario)

---
+++ src/main/java/com/uniovi/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.uniovi.controllers;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import com.uniovi.entities.*;
+import com.uniovi.entities.types.Rol;
+import com.uniovi.services.SecurityService;
+import com.uniovi.services.UserService;
+import com.uniovi.validators.SignUpFormValidator;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private SecurityService securityService;
+
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String signup(@Validated User user, BindingResult result) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "signup";
+		}
+		user.setRol(Rol.STAND);
+		userService.addUser(user);
+		securityService.autoLogin(user.getEmail(), user.getRepassword());
+		return "redirect:home";
+	}
+
+	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	public String home(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String email = auth.getName();
+		User activeUser = userService.getUserByEmail(email);
+		return "home";
+	}
+}
